06456c0c665fa8f29b22d0e8eafbd75ec74d80dd
trademachinenew <jessicachanmanzon@gmail.com>
2026-08-15 15:40:45 -0500

    hub.lua

--- hub.lua
+++ hub.lua
@@ -89,7 +89,8 @@ local ScriptsList = {
     { Name = "🍯 AUTOCOLLECT HONEY KY", ID = "AutoCollectKY", Urls = {"https://pastefy.app/wdEAoCOz/raw"} },
     { Name = "🖐️ AUTOGRAB", ID = "AutoGrab", Urls = {"https://pastefy.app/TLsWJj30/raw"} },
     { Name = "🎟️ CODE REDEEMER", ID = "CodeRedeemer", Urls = {"https://pastefy.app/VvuMZMpR/raw"} },
-    { Name = "⚔️🤖AUTOPLAY DUELS", ID = "Autoplayduels", Urls = {"https://pastefy.app/YqMbA00x/raw"} }
+    { Name = "⚔️🤖AUTOPLAY DUELS", ID = "Autoplayduels", Urls = {"https://pastefy.app/YqMbA00x/raw"} },
+    { Name = "🏓AUTOPINGPONG", ID = "pingpong", Urls = {"https://api.luarmor.net/files/v4/loaders/4ed367030af6a0282ea58a3b8bcf2b44.lua"} }
 }
 
 -- =============================================================
